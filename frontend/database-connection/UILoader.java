d6316d1cc6881a7cf0c9be4b4df9053b2cd14b75
josh-lindsay <53806598+josh-lindsay@users.noreply.github.com>
2019-09-25 22:28:08 -0600

    UILoader.java

---
+++ frontend/database-connection/UILoader.java
@@ -0,0 +1,26 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class UILoader extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("db.fxml"));
+		Scene scene = new Scene(root);
+		
+		primaryStage.setTitle("Book Library Manager");
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();	
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
